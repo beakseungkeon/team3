dd452f71ae73a6fe1c6818b888a6ba31c1079751
pjh1202 <qkrwlsgus147@naver.com>
2024-01-05 16:32:38 +0900

    Program 인터페이스 추가

--- Group3_Homework/src/HomeWorkEx1/HomeWorkProgram.java
+++ Group3_Homework/src/HomeWorkEx1/HomeWorkProgram.java
@@ -3,9 +3,7 @@ package HomeWorkEx1;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import Program.Program;
-import day05.ArrayCopyEx1;
 /* 기한은 1월 5일까지 github에 업로드 후 단톡방에 공유
  * 영어 단어장을 관리하는 프로그램 작성
  *  - 한 단어에 뜻이 여러개 있을 수 있음
