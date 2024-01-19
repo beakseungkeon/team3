ac8b645dad498ca3b919bf3a9e46b8dab5da4590
pjh1202 <qkrwlsgus147@naver.com>
2024-01-19 10:00:39 +0900

    remove 추가

--- java_pjh/src/university/UniversityManage.java
+++ java_pjh/src/university/UniversityManage.java
@@ -1,4 +1,4 @@
-package university;
+package universityTeam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,4 +9,45 @@ public class UniversityManage {
 	List<Department> department = new ArrayList<Department>();
 	List<Lecture> lecture = new ArrayList<Lecture>();
 	
+	boolean insertStd(String name, int age, String department) {
+		return false;
+	}
+	
+	boolean updateStd(int num, String name, int age) {
+		return true;
+	}
+	
+	
+	boolean removeStd(int num) {
+		People stdNum = new People(num);
+		if(!student.contains(stdNum)) {
+			System.out.println("해당학번의 학생이 존재하지 않습니다.");
+			return false;
+		}
+		int index = student.indexOf(stdNum);
+		student.remove(index);
+		return false;
+	}
+	
+	boolean insertDepartment(String name) {
+		return true;
+	}
+	
+	boolean updateDepartment(String name, String name2) {
+		return false;
+		
+	}
+	
+	boolean removeDepartment(String name) {
+		Department dname = new Department(name);
+		if(department.contains(dname)) {
+			int index = department.indexOf(dname);
+			department.remove(index);
+			return true;
+		}
+		else {
+			System.out.println("해당되는 이름의 학과가 없습니다.");
+			return false;
+		}
+	}
 }
--- java_pjh/src/university/UniversityProgram.java
+++ java_pjh/src/university/UniversityProgram.java
@@ -1,4 +1,4 @@
-package university;
+package universityTeam;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -56,6 +56,7 @@ public class UniversityProgram implements Program {
 		case 1: break;
 		case 2: break;
 		case 3: break;
+		case 4: break;
 		default: new InputMismatchException(); break;
 		}
 		
@@ -66,10 +67,76 @@ public class UniversityProgram implements Program {
 		int menu = scanner.nextInt();
 		switch(menu) {
 		case 1: printDepartmentMenu(); break;
-		case 2: break;
-		case 3: break;
+		case 2: printStudentMenu(); break;
+		case 3: printProfessorMenu(); break;
 		default: new InputMismatchException(); break;
 		}
+		
+	}
+
+	private void printProfessorMenu() {
+		pm.printSubMenu3_3();
+		int menu = scanner.nextInt();
+		switch(menu) {
+		case 1: insertProfessor(); break;
+		case 2: updateProfessor(); break;
+		case 3: removeProfessor(); break;
+		case 4: showProfessor(); break;
+		default: new InputMismatchException(); break;
+		}
+	}
+
+	private void insertProfessor() {
+		
+	}
+
+	private void updateProfessor() {
+		
+		
+	}
+
+	private void removeProfessor() {
+		
+		
+	}
+
+	private void showProfessor() {
+		
+		
+	}
+
+	private void printStudentMenu() {
+		pm.printSubMenu3_2();
+		int menu = scanner.nextInt();
+		switch(menu) {
+		case 1: insertStudent(); break;
+		case 2: updateStudent(); break;
+		case 3: removeStudent(); break;
+		case 4: showStudent(); break;
+		default: new InputMismatchException(); break;
+		}
+	}
+
+	private void showStudent() {
+		
+	}
+
+	private void insertStudent() {
+		
+	}
+
+	private void updateStudent() {
+		
+	}
+
+	private void removeStudent() {
+		System.out.print("삭제할 학생의 학번을 입력하세요 : ");
+		int num = scanner.nextInt();
+		if(um.removeStd(num)) {
+			System.out.println("정상적으로 삭제되었습니다.");
+			return;
+		}
+		System.out.println("삭제 실패");
 		
 	}
 
@@ -80,21 +147,38 @@ public class UniversityProgram implements Program {
 		case 1: insertDepartment(); break;
 		case 2: updateDepartment(); break;
 		case 3: removeDepartment(); break;
+		case 4: showDepartment(); break;
 		default: new InputMismatchException(); break;
 		}
 	}
 
-	private void insertDepartment() { //과 등록
-		}
+	private void showDepartment() {
+		
+	}
 
-	private void updateDepartment() { //과 수정
+	private void insertDepartment() {
+		
+	}
+
+	private void updateDepartment() {
 		
 	}
 
 	private void removeDepartment() { //과 삭제
-		
+		//과 목록에 값이 존재하지 않으면 입력되어 있는 과가 없습니다 출력
+		System.out.println("---과 목록---");
+		System.out.print("[");
+		for(int i=0; i<um.department.size(); i++) {
+			System.out.print(um.department.get(i).name+",  ");
+		}
+		System.out.print("]");
+		System.out.print("\n삭제할 과의 이름을 입력하세요 : ");
+		String user = scanner.next();
+		if(um.removeDepartment(user)) {
+			System.out.println("정상적으로 삭제되었습니다.");
+			return;
+		}
+		System.out.println("입력 오류");
 	}
 	
-	
-
 }
