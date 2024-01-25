5f1e448f9420294d3f9409ee3d3f504e9f8700e6
adsihn <153248298+adsihn@users.noreply.github.com>
2024-01-25 16:03:03 +0900

    학생,학과 등록

---
+++ src/university/UniversityManage.java
@@ -0,0 +1,52 @@
+package university;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversityManage {
+	List<People> student = new ArrayList<People>();
+	List<People> professor = new ArrayList<People>();
+	List<department> department = new ArrayList<department>();
+	List<Lecture> lecture = new ArrayList<Lecture>();
+	
+	public void InsertStudent(People std) {
+		if(student.contains(std)) {
+			System.out.println("이미 등록된 학생입니다.");
+			return;
+		}
+		student.add(std);
+		System.out.println("학생을 등록했습니다.");
+	}
+	
+	public void InsertDepartment(department dp) {
+		if(student.contains(dp)) {
+			System.out.println("이미 등록된 학과입니다.");
+			return;
+		}
+		department.add(dp);
+		System.out.println("학과를 등록했습니다.");
+	}
+	
+	public void ChangeDepartment(String dpName,String newDpName,int NewDpCode ) {
+		for(int i=0; i<department.size();i++) {
+			if(department.get(i).getName().equals(dpName)) {
+				department.get(i).setName(newDpName);
+				department.get(i).setCode(NewDpCode);
+			}
+			else {
+				System.out.println("해당하는 이름의 학과가 없습니다.");
+			}
+		}
+	}
+	public void ChangeStudent(String name,String newName,int newAge,int newNum) {
+		for(int i=0;i<student.size();i++) {
+			if(student.get(i).getName().equals(name)) {
+				student.get(i).setName(newName);
+				student.get(i).setAge(newAge);
+				
+			}
+		}
+		
+		
+	}
+}
---
+++ src/university/UniversityProgram.java
@@ -0,0 +1,158 @@
+package university;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import program.Program;
+import university.service.PrintMenu;
+
+public class UniversityProgram implements Program {
+	
+	PrintMenu pm = new PrintMenu();
+	UniversityManage um = new UniversityManage();
+	private Scanner scanner = new Scanner(System.in);
+	@Override
+	public void run() {
+		int menu=0;
+		do {
+			printMenu();
+			try {
+				menu=scanner.nextInt();
+				runMenu(menu);
+			} catch (InputMismatchException e) {
+				System.out.println("잘못된 입력입니다.");
+				scanner.nextLine();
+			}
+		}while(menu!=4);
+	}
+
+	@Override
+	public void printMenu() {
+		pm.printMain();
+	}
+
+	@Override
+	public void runMenu(int menu) {
+		switch(menu) {
+		case 1: studentManage(); break;
+		case 2: professorManage(); break;
+		case 3: Manager(); break;
+		default: new InputMismatchException(); break;
+		}
+	}
+
+	private void studentManage() {
+		pm.printSubMenu1();
+		int menu = scanner.nextInt();
+		switch(menu) {
+		case 1: break;
+		case 2: break;
+		default: new InputMismatchException(); break;
+		}
+	}
+
+	private void professorManage() {
+		pm.printSubMenu2();
+		int menu = scanner.nextInt();
+		switch(menu) {
+		case 1: break;
+		case 2: break;
+		case 3: break;
+		default: new InputMismatchException(); break;
+		}
+		
+	}
+
+	private void Manager() {
+		pm.printSubMenu3();
+		int menu = scanner.nextInt();
+		switch(menu) {
+		case 1: printDepartmentMenu(); break;
+		case 2: printStudentMenu(); break;
+		case 3: break;
+		default: new InputMismatchException(); break;
+		}
+		
+	}
+
+	private void printStudentMenu() {
+		// TODO Auto-generated method stub
+		pm.printSubMenu3_2();
+		int menu = scanner.nextInt();
+		switch(menu) {
+		case 1: insertStudent(); break;
+		case 2: updateStudent(); break;
+		case 3: removeStudent(); break;
+		default: new InputMismatchException(); break;
+		}
+	}
+
+	private void removeStudent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void updateStudent() {
+		// TODO Auto-generated method stub
+		scanner.nextLine();
+		System.out.println("수정할 학생 이름:");
+		String Name=scanner.nextLine();
+		System.out.println("새로운 학생 이름");
+		String newName=scanner.nextLine();
+		System.out.println("새로운 나이:");
+		int newAge=scanner.nextInt();
+		System.out.println("새로운 학번:");
+		int newNum=scanner.nextInt();
+		um.ChangeStudent(Name, newName, newAge, newNum);
+	}
+
+	private void insertStudent() { //학생 등록
+		scanner.nextLine();
+		System.out.println("등록할 학생 이름:");
+		String name=scanner.nextLine();
+		System.out.println("나이:");
+		int age=scanner.nextInt();
+		System.out.println("학번:");
+		int num=scanner.nextInt();
+		System.out.println("학과:");
+		String department=scanner.nextLine();
+		People std = new People(name, age, num, department, null);
+		um.InsertStudent(std);
+	}
+
+	private void printDepartmentMenu() {
+		pm.printSubMenu3_1();
+		int menu = scanner.nextInt();
+		switch(menu) {
+		case 1: insertDepartment(); break;
+		case 2: updateDepartment(); break;
+		case 3: removeDepartment(); break;
+		default: new InputMismatchException(); break;
+		}
+	}
+
+	private void insertDepartment() { //과 등록
+		scanner.nextLine();
+		System.out.println("등록할 학과명:");
+		String dpName=scanner.nextLine();
+		System.out.println("학과 코드:");
+		int code=scanner.nextInt();
+		department dp = new department(dpName, code, null, null);
+		um.InsertDepartment(dp);
+		}
+
+	private void updateDepartment() { //과 수정
+		scanner.nextLine();
+		System.out.println("수정하실 학과 이름:");
+		String dpName=scanner.nextLine();
+		System.out.println("새로운 학과 이름");
+		String newDpName=scanner.nextLine();
+		System.out.println("새로운 코드:");
+		int newCode=scanner.nextInt();
+		um.ChangeDepartment(dpName, newDpName, newCode);
+	}
+
+	private void removeDepartment() { //과 삭제
+		
+	}
+}
