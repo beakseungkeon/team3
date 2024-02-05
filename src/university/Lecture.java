a93dbf352edd3fe59493a372170c4b23e27a6351
pjh1202 <153247887+pjh1202@users.noreply.github.com>
2024-02-05 10:01:11 +0900

    Delete src/university directory

--- src/university/Lecture.java
+++
@@ -1,8 +0,0 @@
-package university;
-
-import java.util.HashMap;
-
-public class Lecture {
-	String Professor;
-	HashMap<String,Integer>student=new HashMap<String,Integer>();
-}
--- src/university/service/UniversityServiceImp.java
+++
@@ -1,5 +0,0 @@
-package university.service;
-
-public class UniversityServiceImp {
-
-}
